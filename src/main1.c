c2a051de88cf92c5b1fec84ef25076446bdcb82f
Chris Jones <chris@embeddedocean.com>
2017-05-28 20:16:10 -0700

    changed GPS input prompt

--- src/main1.c
+++ src/main1.c
@@ -97,35 +97,39 @@ int main (void)
 	// read upload interval
 	shutdown_pi_after_upload = (uint8_t)console_prompt_int("Shutdown PI after upload (1=yes, 0=no)", shutdown_pi_after_upload, timeout);
 
-	// read and set date and time
-	fprintf(stdout, "\r\nLooking for GPS Time on UART1\r\n" );
+	// GPS read and set date and time
 	rtc_time_t rtc = {.century=20, .year=17, .month=5, .day=26, .hour=1, .minute=0, .second=0};
 	ioport_set_pin_level(LED_PIN, LED_ON);
 	uart_configure_queue(GPS_PORT, GPS_UART_BAUDRATE);
 	uart_set_termination(GPS_PORT, USART_NEWLINE_TERMINATION);  // * termination
 	uart_start_queue(GPS_PORT);
-	int gps_timeout = 10000; // 10 seconds
-	while( gps_timeout ) {
-		ioport_toggle_pin_level(LED_PIN);
-		uint8_t str[128];
-		enum status_code status = uart_read_message_queue(GPS_PORT, str, 128);
-		// if a ZDA message is found, use it to set the time
-		if( status == STATUS_OK && gps_parse_zda(str, &rtc)) {
-			// sometimes a bad time is found, so keep looking
-			if(rtc.year > 06) {
-				fprintf(stdout, "Found GPS message: %s\r\n", str);
-				break;
+	int gps_timeout = 60000; // 60 seconds
+	if( console_prompt_int("Set time with GPS connected to UART1 (1=yes, 0=no)", 1, timeout) ) {
+		fprintf(stdout, "\r\nLooking for GPS Time on UART1 (timeout in %d seconds)\r\n", gps_timeout );
+		while( gps_timeout ) {
+			ioport_toggle_pin_level(LED_PIN);
+			uint8_t str[128];
+			enum status_code status = uart_read_message_queue(GPS_PORT, str, 128);
+			// if a ZDA message is found, use it to set the time
+			if( status == STATUS_OK && gps_parse_zda(str, &rtc)) {
+				// sometimes a bad time is found, so keep looking
+				if(rtc.year > 06) {
+					fprintf(stdout, "Found GPS message: %s\r\n", str);
+					break;
+				}
+			} else {
+				gps_timeout--;
+				delay_ms(1);
 			}
-		} else {
-			gps_timeout--;
-			delay_ms(1);
 		}
+		uart_stop_queue(GPS_PORT);
+	} else {
+		gps_timeout = 0;
 	}
-	uart_stop_queue(GPS_PORT);
 	
-	// if not GPS message is found 
+	// if no GPS message found 
 	if( gps_timeout == 0) {
-		fprintf(stdout, "No GPS Time found.\r\n" );
+		fprintf(stdout, "No GPS Time message found.\r\n" );
 		fprintf(stdout, "Enter time manually using epoch (or Linux) time.\r\n" );
 		uint32_t epoch = rtc_time_to_epoch(&rtc);
 		epoch = (uint32_t)console_prompt_int("Enter epoch time", epoch, timeout);
